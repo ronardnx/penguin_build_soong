e052336addcf311380c620de18d2c21aba3f2923
LuK1337 <priv.luk@gmail.com>
2025-02-19 18:36:13 +0100

    soong: Treat vendor/aospa-priv/* as release-keys
    Change-Id: I7e7f180dda4402ada8b2124d0c26bb844dc2cb5f

--- android/config.go
+++ android/config.go
@@ -1541,6 +1541,9 @@ func (c *config) BuildKeys() string {
 	if defaultCert == "" || defaultCert == filepath.Join(testKeyDir, "testkey") {
 		return "test-keys"
 	}
+	if strings.HasPrefix(defaultCert, "vendor/aospa-priv/") {
+		return "release-keys"
+	}
 	return "dev-keys"
 }
 
